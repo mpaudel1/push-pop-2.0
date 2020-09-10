81f6cd6e7e070a8c4698653cc58b8de9ae7a0df0
epic <mpaudel100@gmail.com>
2020-09-10 15:57:29 -0700

    `

--- Ground.js
+++ Ground.js
@@ -9,9 +9,11 @@ class Ground {
       World.add(world, this.body);
     }
     display(){
+      push();
       var pos =this.body.position;
       rectMode(CENTER);
       fill("brown");
       rect(pos.x, pos.y, this.width, this.height);
+      pop();
     }
   };
